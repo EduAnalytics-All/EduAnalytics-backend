4bac53be0e8aa1230861ad1994b42a8195c811f8
Danilo505 <danilo.dossantos@jala.university>
2025-12-22 19:31:47 -0300

    feat: add user registration validations and role resolution

--- src/main/java/com/project/EduAnalytics_backend/services/UserService.java
+++ src/main/java/com/project/EduAnalytics_backend/services/UserService.java
@@ -18,16 +18,26 @@ public class UserService {
 
     public User register(String name, String email, String password, String type) {
 
-        if (name == null || name.isBlank())
-            throw new IllegalArgumentException("Required name");
+        if (name == null || name.isBlank() || name.trim().length() <2)
+            throw new IllegalArgumentException("Name must have at least 2 characters");
         if (email == null || email.isBlank())
             throw new IllegalArgumentException("Required email");
         if (password == null || password.length() < 8)
             throw new IllegalArgumentException("Weak password (min 8)");
         if (type == null || type.isBlank())
-            throw new IllegalArgumentException("Required type (ESTUDANTE or PROFESSOR)");
+            throw new IllegalArgumentException("Required type (ALUNO or PROFESSOR)");
 
         String normalizedEmail = email.trim().toLowerCase();
+
+        if (!normalizedEmail.contains("@")){
+            throw new IllegalArgumentException("Email must contain '@'");
+        }
+
+        int atIndex = normalizedEmail.indexOf("@");
+
+        if (!normalizedEmail.substring(atIndex).endsWith(".com")) {
+            throw new IllegalArgumentException("Email must end with '.com' after '@'");
+        }
 
         if (userRepository.existsByEmail(normalizedEmail)) {
             throw new IllegalStateException("Email already registered");
